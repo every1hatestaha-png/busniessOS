fc7e637c845f5f6c4e15812f89be9cd22a5fedd1
Taha khizar <every1hatestaha@gmail.com>
2026-09-19 21:11:32 +0500

    Fix Prisma runtime import in fbr-remote-submission.ts

--- lib/server/fbr-remote-submission.ts
+++ lib/server/fbr-remote-submission.ts
@@ -1,6 +1,6 @@
 import "server-only";
 
-import type { Prisma } from "@prisma/client";
+import { Prisma } from "@prisma/client";
 
 import { postInvoiceToFbr, type FbrRemoteResult } from "@/lib/fbr/client";
 import type { FbrInvoicePayload } from "@/lib/fbr/digital-invoicing";
